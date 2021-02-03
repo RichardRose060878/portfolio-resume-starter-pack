5a0d352276b7fef91e1ab324204f95db69f513c8
Richard <richardrose060878@gmail.com>
2021-02-03 17:19:32 +0000

    uploaded portfolio

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React from 'react'
 
 const Footer = ({ data }) => {
   if (data) {
@@ -9,33 +9,43 @@ const Footer = ({ data }) => {
             <i className={network.className}></i>
           </a>
         </li>
-      );
-    });
+      )
+    })
   }
 
   return (
     <footer>
-      <div className="row">
-        <div className="twelve columns">
-          <ul className="social-links">{networks}</ul>
+      <div className='row'>
+        <div className='twelve columns'>
+          <ul className='social-links'>{networks}</ul>
+          <ul>
+            <li>Modified by Richard Rose</li>
+          </ul>
 
-          <ul className="copyright">
+          <ul className='copyright'>
             <li>
-              Made by{" "}
-              <a title="PAPA" href="http://www.papareact.com/">
+              Made by{' '}
+              <a title='PAPA' href='http://www.papareact.com/'>
                 PAPA
+              </a>
+            </li>
+
+            <li>
+              Design by{' '}
+              <a title='Styleshout' href='http://www.styleshout.com/'>
+                Styleshout
               </a>
             </li>
           </ul>
         </div>
-        <div id="go-top">
-          <a className="smoothscroll" title="Back to Top" href="#home">
-            <i className="icon-up-open"></i>
+        <div id='go-top'>
+          <a className='smoothscroll' title='Back to Top' href='#home'>
+            <i className='icon-up-open'></i>
           </a>
         </div>
       </div>
     </footer>
-  );
-};
+  )
+}
 
-export default Footer;
+export default Footer
